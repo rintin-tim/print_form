38d595a048b1e8a948170de488806ed231c756e6
Tim Morris <Tim>
2022-08-26 23:44:56 +0100

    styling

--- print_form.js
+++ print_form.js
@@ -1,23 +1,22 @@
 "use strict";
 
-const print_inputs = function (includeHidden) {
+const print_inputs = function (includeHidden = false) {
   let finalString = "";
   let htmlTable = `
   <html>
-  <p>Time: ${new Date()}</p>
-  <p>URL: ${window.location.href}</p>
-  <table border=1 cellpadding=3>
-	<tbody>
-        <tr>
-            <td>Name</td>
-            <td>Value</td>
-            <td>Hidden</td>
-        </tr>
-    {{table_rows_here}}
-		
-	</tbody>
+    <p>Time: ${new Date()}</p>
+    <p>URL: ${window.location.href}</p>
+    <table border=1 cellpadding=3>
+	    <tbody>
+            <tr>
+                <td>Name</td>
+                <td>Value</td>
+                <td>Hidden</td>
+            </tr>
+            {{table_rows_here}}
+	    </tbody>
     </table>
-    </html>
+  </html>
   `;
 
   function logElement(element) {
@@ -26,7 +25,6 @@ const print_inputs = function (includeHidden) {
     <tr>
         <td>${element.name}</td>
         `;
-      //   finalString += element.name;
     } else {
       finalString += `
     <tr>
@@ -35,17 +33,15 @@ const print_inputs = function (includeHidden) {
     }
     finalString += `<td>${
       element.hasAttribute("aria-checked")
-        ? `aria-checked: ${element.getAttribute("aria-checked")} ${
+        ? `[aria-checked]=${element.getAttribute("aria-checked")}  (${
             element.value
-          }`
+          })`
         : `${element.value}`
     }</td>`;
-    // finalString += "\n";
     if (element.type == "hidden") {
       finalString += `
         <td>true</td>
         `;
-      //   finalString += " || ** HIDDEN FIELD **";
     } else {
       finalString += `
         <td>false</td>
@@ -67,10 +63,8 @@ const print_inputs = function (includeHidden) {
   let newWindow = window.open("about:blank");
   if (newWindow) {
     let htmlOutput = htmlTable.replace("{{table_rows_here}}", finalString);
-    // const htmlOutput = finalString.replace(/(?:\r\n|\r|\n)/g, "<br/>");
-
     newWindow.document.write(htmlOutput);
     newWindow.stop();
   }
 };
-print_inputs(false);
+print_inputs();
