a087bce526cce5edf6ca8933d78d99f98ec0f672
Tim Morris <Tim>
2022-08-26 21:59:52 +0100

    implemented strict mode

--- print_form.js
+++ print_form.js
@@ -1,42 +1,47 @@
-print_inputs = function(includeHidden) {
-    var finalString = '';
-    var currentUrl = "Page location is: " + window.location.href;
-    finalString += currentUrl + '\n' + '\n';
-    var time_date = new Date();
-    finalString += "Current time is: " + time_date + '\n' + '\n';
+"use strict";
 
-    function logElement (element) {
-		
-		if (element.name) {
-			finalString += element.name 
-		};
-		if (element.type == 'hidden') {
-			finalString += " || ** HIDDEN FIELD **" 
-		};  
-			finalString += ` ----> ${element.hasAttribute('aria-checked') ? 
-				`aria-checked: ${element.getAttribute('aria-checked')} value: ${element.value}` : 
-			`value: ${element.value}`}`
-		finalString += '\n';
-	}
+const print_inputs = function (includeHidden) {
+  let finalString = "";
+  const currentUrl = "Page location is: " + window.location.href;
+  finalString += currentUrl + "\n" + "\n";
+  const time_date = new Date();
+  finalString += "Current time is: " + time_date + "\n" + "\n";
 
-    result = document.querySelectorAll('input, textarea');
-    result.forEach(function(element) {
-        if (element.value) {
-            if (includeHidden == true) {
-                logElement(element);
-            } else {
-                if (element.type !== 'hidden') {
-                    logElement(element);
-                };
-            };
-        };
-    });
+  function logElement(element) {
+    if (element.name) {
+      finalString += element.name;
+    }
+    if (element.type == "hidden") {
+      finalString += " || ** HIDDEN FIELD **";
+    }
+    finalString += ` ----> ${
+      element.hasAttribute("aria-checked")
+        ? `aria-checked: ${element.getAttribute("aria-checked")} value: ${
+            element.value
+          }`
+        : `value: ${element.value}`
+    }`;
+    finalString += "\n";
+  }
 
-    var newWindow = window.open("about:blank");
-    if (newWindow) {
-	var htmlOutput = finalString.replace(/(?:\r\n|\r|\n)/g, '<br/>'); 
-        newWindow.document.write(htmlOutput);
-        newWindow.stop();        
-    };    
+  const result = document.querySelectorAll("input, textarea");
+  result.forEach(function (element) {
+    if (element.value) {
+      if (includeHidden == true) {
+        logElement(element);
+      } else {
+        if (element.type !== "hidden") {
+          logElement(element);
+        }
+      }
+    }
+  });
+
+  let newWindow = window.open("about:blank");
+  if (newWindow) {
+    const htmlOutput = finalString.replace(/(?:\r\n|\r|\n)/g, "<br/>");
+    newWindow.document.write(htmlOutput);
+    newWindow.stop();
+  }
 };
 print_inputs(false);
