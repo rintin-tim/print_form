9d76e8d62a602bb84204e6a40e93df54cc46f444
Tim Morris <Tim>
2022-08-31 00:28:04 +0100

    use offsetParent for hidden elements. add images for visibility. table styling improvements.

--- print_form.js
+++ print_form.js
@@ -2,23 +2,41 @@
 
 const print_inputs = function (includeHidden = false) {
   let finalString = "";
+
+  // html table structure
   let htmlTable = `
   <html>
-    <p>Time: ${new Date()}</p>
-    <p>URL: ${window.location.href}</p>
-    <table border=1 cellpadding=3">
-	    <tbody>
-            <tr>
-                <td>Name</td>
-                <td>Value</td>
-                <td>Hidden</td>
-            </tr>
-            {{table_rows_here}}
-	    </tbody>
-    </table>
+    <head>
+      <style>
+        table, th, td {
+          padding: 5px;
+          border: 1px solid black;
+        }
+        th {
+          background-color: #96D4D4;
+        }
+      </style>
+    </head>
+    <body>
+      <p>Time: ${new Date()}</p>
+      <p>URL: ${window.location.href}</p>
+      <table>
+        <thead>
+              <tr>
+                  <th>Element Name</th>
+                  <th>Value</th>
+                  <th>Visible</th>
+              </tr>
+        </thead>
+        <tbody>
+              {{table_rows_here}}
+        </tbody>
+      </table>
+    </body>  
   </html>
   `;
 
+  // html row structure (1 per element)
   function logElement(element) {
     if (element.name) {
       finalString += `
@@ -31,6 +49,7 @@ const print_inputs = function (includeHidden = false) {
         <td></td>
         `;
     }
+    // look for aria-checked (checkbox and radio button selection)
     finalString += `<td>${
       element.hasAttribute("aria-checked")
         ? `[aria-checked]=${element.getAttribute("aria-checked")}  (${
@@ -38,28 +57,32 @@ const print_inputs = function (includeHidden = false) {
           })`
         : `${element.value}`
     }</td>`;
-    if (element.type == "hidden") {
+    if (element.offsetParent === null) {
+      // image courtesy <a href="https://www.flaticon.com/free-icons/eye" title="eye icons">Eye icons created by Freepik - Flaticon</a>
       finalString += `
-        <td>true</td>
-        `;
+      <td><img src="https://i.ibb.co/t8ZV5VD/hidden.png" title="Element hidden" alt="Element hidden"></td>
+      `;
     } else {
+      // <a href="https://www.flaticon.com/free-icons/eye" title="eye icons">Eye icons created by torskaya - Flaticon</a>
       finalString += `
-        <td>false</td>
+      <td><img src="https://i.ibb.co/r4Vy0Cp/view.png" title="Element visible" alt="Element visible"></td>
         `;
     }
   }
 
+  // select all input and textarea elements
   const result = document.querySelectorAll("input, textarea");
   result.forEach(function (element) {
     if (includeHidden == true) {
       logElement(element);
     } else {
-      if (element.type !== "hidden") {
+      if (element.offsetParent !== null) {
         logElement(element);
       }
     }
   });
 
+  // insert elements into html table structure and open in new window
   let newWindow = window.open("about:blank");
   if (newWindow) {
     let htmlOutput = htmlTable.replace("{{table_rows_here}}", finalString);
@@ -67,4 +90,4 @@ const print_inputs = function (includeHidden = false) {
     newWindow.stop();
   }
 };
-print_inputs();
+print_inputs(true);
